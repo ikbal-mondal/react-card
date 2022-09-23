73a6f242fb782e7c00215789949950f522475146
ikbal-mondal <kbalmondal062@gmail.com>
2022-09-23 20:52:34 +0530

    added

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -7,7 +7,7 @@ const Card = (props) => {
   <figure><img src={props?.photo?.thumbnailUrl} alt="Shoes" /></figure>
   <div className="card-body">
     <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <p>Hello how are you</p>
     <div className="card-actions justify-end">
       <button className="btn btn-primary">Buy Now</button>
     </div>
